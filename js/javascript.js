6c9a7cc098ae17dec07d0d3108cb6bec756f481e
Stefan0010 <dphoenix32@yahoo.com>
2017-02-01 02:47:49 +0900

    adding check for inputs

--- js/javascript.js
+++ js/javascript.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
       // button_dec = $('#btn-decrypt'),
       button_calc = $('#btn-calc'),
       button_conv = $('#btn-conv'),
+
       // privateExponent =  
       // modulus = prime1*prime2,
       // encryptCoef = privateExponent % 
@@ -15,108 +16,187 @@ $(document).ready(function () {
       // tao = (prime1-1)*(prime2-1),
      isClosed = false;
 
-     button_inp.click(function() {
-        
-     });
+   button_inp.click(function() {
+      
+   });
 
-     button_conv.click(function() {
-      var converted = convert_text();
-      $('#convert').text(converted);
+   button_conv.click(function() {
+    var converted = convert_text();
+    $('#convert').text(converted);
 
-    });
+  });
 
-     button_calc.click(function(){
-      var num = $('#convert').text();
-        $('#n').val(calc()[0]);
-        $('#Φn').val(calc()[1]);
-        $('#d').val(calc()[2]);
-        $('#btn-keygen').removeAttr('disabled');
+   button_calc.on('click',function(){
+    var num = parseInt($('#convert').text()),
+        tag = '#alert_modulo',
+        message = '<strong>Warning!</strong> your modulo (n) is smaller than the encrypted message!';
 
-    });
-
-     button_key.click(function() {
-      var e =$('#e').val(),
-          p =$('#p').val(),
-          q =$('#q').val(),
-          n =$('#n').val(),
-          enc = calc()[3],
-          msg = $('#inp').val();
-      $('#e-out').text(e);
-      $('#p-out').text(p);
-      $('#q-out').text(q);
-      $('#n-out').text(n);
-      $('#out').text(msg); 
-      $('#enc-out').text(enc);
-        
-    });
-
-     button_enc.click(function() {
-      var enc = $('#enc-out').text(),
-          dec = calc()[4],
-          msg = $('#inp').val(),
-          d =$('#d').val(),
-          n =$('#n').val();
-
-      $('#encrypted').text(enc);
-      $('#n-out2').text(n);
-      $('#d-out').text(d); 
-      $('#result2').text(dec);
-      $('#converted').text(msg);
-     });
-
-
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-
-    function convert_text(){
-    var enc="",
-        m = $('#inp').val(),
-        str="";
-    str = m.toString();
-    for (var i =0;i< m.length;i++){
-      var block = m.charCodeAt(i);
-      enc = enc + block;
+    if (!verify()) {
+      $('#n').val('');
+      $('#Φn').val('');
+      $('#d').val('');
+      bootstrap_alert.fade(tag);
+      return 0;
     }
-    return enc;
+    $('#n').val(calc()[0]);
+    $('#Φn').val(calc()[1]);
+    $('#d').val(calc()[2]);
+    if (num > $('#n').val()) {
+      $('#btn-keygen').prop('disabled',true);
+      bootstrap_alert.warning(tag,message);
+    }
+    else{
+      bootstrap_alert.fade(tag);
+      $('#btn-keygen').prop('disabled',false);
+    }
+  });
+
+   button_key.click(function() {
+    var e =$('#e').val(),
+        p =$('#p').val(),
+        q =$('#q').val(),
+        n =$('#n').val(),
+        enc = calc()[3],
+        msg = $('#inp').val();
+    $('#e-out').text(e);
+    $('#p-out').text(p);
+    $('#q-out').text(q);
+    $('#n-out').text(n);
+    $('#out').text(msg); 
+    $('#enc-out').text(enc);
+      
+  });
+
+   button_enc.click(function() {
+    var enc = $('#enc-out').text(),
+        dec = calc()[4],
+        msg = $('#inp').val(),
+        d =$('#d').val(),
+        n =$('#n').val();
+
+    $('#encrypted').text(enc);
+    $('#n-out2').text(n);
+    $('#d-out').text(d); 
+    $('#result2').text(dec);
+    $('#converted').text(msg);
+   });
+
+
+  $('#e').keyup(function() {
+    var e =$('#e').val(),
+        p =$('#p').val(),
+        q =$('#q').val();
+    if(e.length ===0 || p.length === 0 || q.length === 0){
+      $('#btn-calc').prop('disabled',true);
+    }
+    else{
+      $('#btn-calc').prop('disabled',false);
+    }
+  })
+
+  $('#p').keyup(function() {
+    var e =$('#e').val(),
+        p =$('#p').val(),
+        q =$('#q').val();
+    if(e.length ===0 || p.length === 0 || q.length === 0){
+      $('#btn-calc').prop('disabled',true);
+    }
+    else{
+      $('#btn-calc').prop('disabled',false);
+    }
+  })
+
+  $('#q').keyup(function() {
+    var e =$('#e').val(),
+        p =$('#p').val(),
+        q =$('#q').val();
+    if(e.length ===0 || p.length === 0 || q.length === 0){
+      $('#btn-calc').prop('disabled',true);
+    }
+    else{
+      $('#btn-calc').prop('disabled',false);
+    }
+  })
+
+  trigger.click(function () {
+    hamburger_cross();      
+  });
+
+  function convert_text(){
+  var enc="",
+      m = $('#inp').val(),
+      str="";
+  str = m.toString();
+  for (var i =0;i< m.length;i++){
+    var block = m.charCodeAt(i);
+    enc = enc + block;
+  }
+  return enc;
+}
+
+  bootstrap_alert = function() {}
+  bootstrap_alert.warning = function(tag,message) {
+    $(tag).show();
+    $(tag).html('<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-hide="alert" aria-label="close" data-dismiss="alert">&times;</button>'+ message + '</div>')
+  }
+  bootstrap_alert.fade = function(tag) {
+    $(tag).hide();
   }
 
-    function calc() {
-      var e = parseInt($('#e').val()),
-          p = parseInt($('#p').val()),
-          q = parseInt($('#q').val()),
-          n = p*q,
-          tao = (p-1)*(q-1),
-          d = tao + Euclid_gcd(tao,e)[2],   
-          num = parseInt($('#convert').text()),
 
-          encryptCoef =  Math.pow(num,e) % n,
-          decryptCoef =  Math.pow(encryptCoef,d) % n,
-          arr = [n,tao,d,encryptCoef,decryptCoef];
-      console.log(num);
-      return arr;
-    }
+  function calc() {
+    var e = parseInt($('#e').val()),
+        p = parseInt($('#p').val()),
+        q = parseInt($('#q').val()),
+        n = p*q,
+        tao = (p-1)*(q-1),
+        d = tao + Euclid_gcd(tao,e)[2],   
+        num = parseInt($('#convert').text()),
 
-    function hamburger_cross() {
-
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
+        encryptCoef =  Math.pow(num,e) % n,
+        decryptCoef =  Math.pow(encryptCoef,d) % n,
+        arr = [n,tao,d,encryptCoef,decryptCoef];
+    // console.log(num);
+    return arr;
   }
+
+  function hamburger_cross() {
+
+    if (isClosed == true) {          
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {   
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+}
 
 
   
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
   });
+
+  function verify() {
+    var e =  parseInt($('#e').val()),
+        p =  parseInt($('#p').val()),
+        q =  parseInt($('#q').val()),
+        tag_out = '#alert_integer',
+        message = 'Please input an integer!';
+    if (e !== parseInt(e,10)|| p !== parseInt(p,10) ||q !== parseInt(q,10)) {
+      bootstrap_alert.warning(tag_out,message);
+      return false;
+    }
+    else {
+      bootstrap_alert.fade(tag_out);
+      return true;
+    }
+
+  }
 
   function Euclid_gcd(a, b) {
     a = +a;
